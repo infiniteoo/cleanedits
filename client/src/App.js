b81b1ab7b763ef4fce274824ca5525958906d637
Troy Dorman <troydorman@gmail.com>
2021-04-25 19:00:58 -0600

    created the inputform component as well as the payment component

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import tupacBG from './imgs/2pac.jpg'
-import colorsBG from './imgs/colors.jpg'
-import concert1BG from './imgs/concert1.jpg'
-import graf1BG from './imgs/graf1.jpg'
-import guitarBG from './imgs/guitar.jpg'
-
+import tupacBG from "./imgs/2pac.jpg";
+import colorsBG from "./imgs/colors.jpg";
+import concert1BG from "./imgs/concert1.jpg";
+import graf1BG from "./imgs/graf1.jpg";
+import guitarBG from "./imgs/guitar.jpg";
+import InputForm from "./components/InputForm";
+import Payment from "./components/Payment";
+import { Button } from "@material-ui/core";
 
 import FilesUpload from "./components/FilesUpload";
 
 function App() {
-
   const randomBG = () => {
-
-    const bgArray = [tupacBG, concert1BG, colorsBG, graf1BG, guitarBG]
+    const bgArray = [tupacBG, concert1BG, colorsBG, graf1BG, guitarBG];
     const bg = bgArray[Math.floor(Math.random() * bgArray.length)];
     return bg;
+  };
 
-  }
+  const [uploaded, setUploaded] = useState(false);
+  const [infoEntered, setInfoEntered] = useState(false);
 
   return (
-    <div className="main-container" style={{backgroundImage: `url(${randomBG()})`}}>
-      <div>
-      <div className="inner-container">
-          <h3>Clean Edits</h3>
-          <h4>max file size 50mb</h4>
-          <FilesUpload />
+    <div
+      className="main-container"
+      style={{ backgroundImage: `url(${randomBG()})` }}
+    >
+      {!uploaded && (
+        <div>
+          <div className="inner-container">
+            <p style={{ fontSize: "105px" }}>Clean Edits</p>
+            <h4>radio friendly edits for your project</h4>
+            <FilesUpload />
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              style={{ marginTop: "10px" }}
+              onClick={()=>setUploaded(true)}
+            >
+              Submit
+             
+            </Button>
+          </div>
         </div>
-
-      
-
+      )}
+      {uploaded && !infoEntered && (
+        <div>
+        <InputForm setInfoEntered={setInfoEntered}/>
+        
       </div>
-       
+      )}
+       {uploaded && infoEntered && (
+        <div>
+        <Payment />
+        
+      </div>
+      )}
+
+      <div></div>
+      
     </div>
   );
 }
---
+++ client/src/components/Payment.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Button,
+  TextField,
+  Grid,
+  Typography,
+  Container,
+  Paper,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    
+  },
+  gridContainer: {
+    width: "100%",
+    justifyContent: "center",
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: "column",
+      
+    },
+  },
+  container: {
+    /* width: "600px", */
+    margin: "5px 0",
+    padding: 0,
+    
+    [theme.breakpoints.down("xs")]: {
+      width: "80%",
+    },
+  },
+  margin: {
+    marginTop: 20,
+  },
+  padding: {
+    padding: 0,
+  },
+  paper: {
+    padding: "10px 20px",
+    border: "2px solid black",
+    marginRight: "10px"
+   
+  },
+}));
+
+const InputForm = (props) => {
+  const classes = useStyles();
+  return (
+    <Container className={classes.container}>
+      <Paper elevation={10} className={classes.paper}>
+        <form className={classes.root} noValidate autoComplete="off">
+          <Grid container className={classes.gridContainer}>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant="h6" style={{fontFamily: 'Freckle Face', fontSize: "50px"}}>
+                Payment
+              </Typography>
+              
+              <Button 
+                variant="contained" 
+                color="primary" 
+                fullWidth 
+                className={classes.margin}
+                onClick={() => props.setInfoEntered(true)}
+                
+                > Submit </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default InputForm;
